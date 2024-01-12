981a502ed45b56aa5011cbea3e10d58928495eb0
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-01-12 14:05:40 +0100

    update data from server to web

--- public/js/eventsweb.js
+++ public/js/eventsweb.js
@@ -2,19 +2,23 @@ function updateCurrentTime() {
   const currentTimeElement = document.getElementById("current-time");
   const now = new Date();
   const hours = now.getHours();
-  const minutes = now.getMinutes().toString().padStart(2, "0"); // Agregar cero al inicio si es necesario
+  const minutes = now.getMinutes().toString().padStart(2, "0"); 
   currentTimeElement.textContent = `${hours}:${minutes}`;
 }
 
-// Update current time every minute
 setInterval(updateCurrentTime, 1);
-
-// Initial update
 updateCurrentTime();
 
-function dividirTexto() {
-  var parrafo = document.getElementById("description-text");
-  var texto = parrafo.textContent;
+function updateEventTime(eventTime) {
+  const eventTimeElement = document.getElementById("event-time");
+  const eventDate = new Date(eventTime);
+  const hours = eventDate.getHours().toString().padStart(2, "0");
+  const minutes = eventDate.getMinutes().toString().padStart(2, "0");
+  eventTimeElement.textContent = `${hours}:${minutes}`;
+}
+
+function dividirTexto(parrafo) {
+  var texto = parrafo
   var palabras = texto.split(" ");
   var lineas = [];
 
@@ -22,7 +26,48 @@ function dividirTexto() {
     var linea = palabras.slice(i, i + 5).join(" ");
     lineas.push(linea);
   }
-
-  parrafo.innerHTML = lineas.join("<br>");
+  return lineas.join("<br>");
 }
-window.onload = dividirTexto;
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/api/list", { method: "POST" })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length > 0) {
+        const evento = data[0];
+        console.log(evento);
+
+        document.getElementById("eventday").innerText = obtenerDia(evento.event_date);
+        document.getElementById("eventdate").innerText = formatoFecha(evento.event_date);
+        updateEventTime(evento.event_date);
+        document.getElementById("description-text").innerText = dividirTexto(evento.desc);
+        document.getElementById("event-title").innerText = evento.title;
+
+        // document.querySelector(".poster").src = evento.thumb_url || "ruta_por_defecto";
+        // document.querySelector(".qr-code img").src = evento.qr_url || "ruta_por_defecto";
+      }
+    })
+    .catch((error) => console.error("Error al obtener datos de la API", error));
+});
+
+function obtenerDia(fecha) {
+  const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  const fechaObj = new Date(fecha);
+  const diaSemana = fechaObj.getDay();
+  return diasSemana[diaSemana];
+}
+
+function formatoFecha(fecha) {
+  const fechaObj = new Date(fecha);
+  const dia = fechaObj.getDate().toString().padStart(2, "0");
+  const mes = (fechaObj.getMonth() + 1).toString().padStart(2, "0");
+  const año = fechaObj.getFullYear();
+  return `${dia}/${mes}/${año}`;
+}
+
+function obtenerHoraActual() {
+  const ahora = new Date();
+  const horas = ahora.getHours().toString().padStart(2, "0");
+  const minutos = ahora.getMinutes().toString().padStart(2, "0");
+  return `${horas}:${minutos}`;
+}
