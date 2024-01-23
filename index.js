ce27106a4e1feeaecf0101568bdfb1b04651ef7e
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-01-23 22:11:22 +0100

    api protection

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
 
 // Rutas del servidor web
 const apiRoutes = require("./routes/apiRoutes");
-app.use("/api", apiRoutes);
+app.use("/api", requireAuth, apiRoutes);
 
 const dashRoutes = require("./routes/dashRoutes");
 app.use("/dash", requireAuth, dashRoutes);
