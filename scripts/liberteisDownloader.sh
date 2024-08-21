6361a6a335fcf724cfebffedbf534673a9e7b34e
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-08-21 23:40:04 +0200

    update downloader

--- scripts/liberteisDownloader.sh
+++ scripts/liberteisDownloader.sh
@@ -1,3 +1,4 @@
+sudo rm liberteisManager.sh
 sudo curl -o liberteisManager.sh https://raw.githubusercontent.com/AlexDeveloperUwU/liberteis/main/scripts/liberteisManager.sh
 sudo chmod +x liberteisManager.sh
 sudo ./liberteisManager.sh
