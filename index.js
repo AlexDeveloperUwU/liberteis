1b4312a1775c2ad93905550a0c51f0556f48ec21
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-09-11 11:40:18 +0200

    update deps + fixes

--- index.js
+++ index.js
@@ -93,8 +93,14 @@ app.use("/", webRoutes);
 
 app.use("/auth", authRouter);
 
-app.get("*", (req, res) => {
-  res.render("errors/404", { t: res.t, lang: req.getLocale() });
+// Middleware para manejar rutas no encontradas (404)
+app.use((req, res, next) => {
+  res.status(404).render("errors/404", { t: res.t, lang: req.getLocale() });
+});
+
+// Middleware para manejar errores internos del servidor (500)
+app.use((err, req, res, next) => {
+  res.status(500).render("errors/500", { t: res.t, lang: req.getLocale() });
 });
 
 // Iniciamos el servidor web express
