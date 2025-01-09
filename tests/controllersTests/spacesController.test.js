13e1edb0d8ff7e23932a0a4fbd372662b38e053c
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-01-09 13:17:03 +0100

    test(tests): :white_check_mark: Added tests for the Spaces controller

---
+++ tests/controllersTests/spacesController.test.js
@@ -0,0 +1,68 @@
+/**
+ * @jest-environment node
+ * @type {import('@jest/globals').Jest}
+ */
+
+import { dbCreateTables } from "../../db/dbController.js";
+import * as spaces from "../../db/spacesController.js";
+
+beforeAll(async () => {
+  await dbCreateTables();
+  const space = await spaces.getSpaceByName("testSpace");
+  if (space !== undefined && space.length > 0) {
+    await spaces.deleteSpace(space[0].id);
+  }
+});
+
+describe("Spaces Controller Tests", () => {
+  let spaceId = null;
+  test("addSpace should insert a new space", async () => {
+    const space = {
+      name: "testSpace",
+      location: "testLocation",
+      info: "testInfo",
+    };
+    await spaces.addSpace(space);
+    const result = await spaces.getSpaceByName("testSpace");
+    expect(result).toBeDefined();
+    spaceId = result[0].id;
+  });
+
+  test("getSpace should return a space", async () => {
+    const result = await spaces.getSpace(spaceId);
+    expect(result).toBeDefined();
+    expect(result[0].name).toBe("testSpace");
+    expect(result[0].location).toBe("testLocation");
+    expect(result[0].info).toBe("testInfo");
+  });
+
+  test("getSpaces should return a list of spaces", async () => {
+    const result = await spaces.getSpaces();
+    expect(result).toBeDefined();
+  });
+
+  test("getSpaceByName should return a space", async () => {
+    const result = await spaces.getSpaceByName("testSpace");
+    expect(result).toBeDefined();
+    expect(result[0].name).toBe("testSpace");
+    expect(result[0].location).toBe("testLocation");
+    expect(result[0].info).toBe("testInfo");
+    expect(result[0].id).toBe(spaceId);
+  });
+
+  test("updateSpace should update a space", async () => {
+    const space = { name: "testSpace updated", location: "updatedLocation", info: "updatedInfo" };
+    await spaces.updateSpace(spaceId, space);
+    const result = await spaces.getSpace(spaceId);
+    expect(result).toBeDefined();
+    expect(result[0].name).toBe("testSpace updated");
+    expect(result[0].location).toBe("updatedLocation");
+    expect(result[0].info).toBe("updatedInfo");
+  });
+
+  test("deleteSpace should remove a space", async () => {
+    await spaces.deleteSpace(spaceId);
+    const space = await spaces.getSpace(spaceId);
+    expect(space).toEqual([]);
+  });
+});
