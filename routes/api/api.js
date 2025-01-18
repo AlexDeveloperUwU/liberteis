ac5d6ed71f557ecd3cd4a770d5fdf32259e0e538
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-01-18 15:40:40 +0100

    feat(api): :sparkles: Added the Categories API

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,6 +2,7 @@ import router from "express";
 import configApi from "./configApi.js";
 import usersApi from "./usersApi.js";
 import spacesApi from "./spacesApi.js";
+import categoriesApi from "./categoriesApi.js";
 
 const apiRouter = router();
 export default apiRouter;
@@ -9,3 +10,4 @@ export default apiRouter;
 apiRouter.use("/config", configApi);
 apiRouter.use("/users", usersApi);
 apiRouter.use("/spaces", spacesApi);
+apiRouter.use("/categories", categoriesApi);
