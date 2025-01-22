5aa5d3c1323317aa755437d06c6850798e92229b
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-01-22 14:21:15 +0100

    feat(db): :sparkles: Created events controller

---
+++ db/eventsController.js
@@ -0,0 +1,31 @@
+import * as dbc from "./dbController.js";
+import * as id from "../utils/idGen.js";
+
+export async function addEvent(event) {
+  event.id = await id.generateId("event");
+  return await dbc.dbSaveData("events", event);
+}
+
+export async function getEvent(id) {
+  return await dbc.dbGetOne("events", id);
+}
+
+export async function getEvents() {
+  return await dbc.dbGetAll("events");
+}
+
+export async function getEventByName(name) {
+  return await dbc.dbGetWhere("events", "name", "=", name);
+}
+
+export async function updateEvent(id, event) {
+  return await dbc.dbUpdateData("events", id, event);
+}
+
+export async function deleteEvent(id) {
+  return await dbc.dbDeleteData("events", id);
+}
+
+export async function checkEventExistence(id) {
+  return await dbc.dbCheckExistence("events", id);
+}
