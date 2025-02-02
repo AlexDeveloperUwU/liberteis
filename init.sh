1cd1c1af9c2be25d5dc39b64c03854b1c04d8aac
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-02-02 21:10:03 +0100

    fix(base): :bug: Fixed init script being used wrongly and rewriting config files

--- init.sh
+++ init.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' 
+
 usage() {
-  echo "Uso: $0 [dev|prod]"
+  echo -e "${YELLOW}Uso: $0 [dev|prod]${NC}"
   exit 1
 }
 
 if [ -n "$1" ]; then
   ENVIRONMENT=$1
 else
-  echo "Seleccione el entorno de desarrollo:"
+  clear
+  echo -e "${BLUE}Seleccione el entorno de desarrollo:${NC}"
   echo "1) Dev"
   echo "2) Prod"
   read -p "Ingrese el número correspondiente (1 o 2): " choice
@@ -21,18 +28,19 @@ else
     ENVIRONMENT="prod"
     ;;
   *)
-    echo "Opción no válida."
+    echo -e "${RED}Opción no válida.${NC}"
     usage
     ;;
   esac
 fi
 
 if [ "$ENVIRONMENT" != "dev" ] && [ "$ENVIRONMENT" != "prod" ]; then
-  echo "Entorno no válido: $ENVIRONMENT"
+  echo -e "${RED}Entorno no válido: $ENVIRONMENT${NC}"
   usage
 fi
 
-echo "Entorno seleccionado: $ENVIRONMENT"
+clear
+echo -e "${GREEN}Entorno seleccionado: $ENVIRONMENT${NC}"
 
 create_directories() {
   directories=(
@@ -44,19 +52,24 @@ create_directories() {
   )
 
   for dir in "${directories[@]}"; do
-    mkdir -p "$dir" || {
-      echo "Error creando el directorio $dir"
-      exit 1
-    }
+    if [ ! -d "$dir" ]; then
+      mkdir -p "$dir" || {
+        echo -e "${RED}Error creando el directorio $dir${NC}"
+        exit 1
+      }
+    fi
   done
 }
 
 set_key() {
-  key=$(openssl rand -hex 32)
-  echo "$key" >"./data/secrets/secret.key" || {
-    echo "Error creando el archivo de clave"
-    exit 1
-  }
+  key_file="./data/secrets/secret.key"
+  if [ ! -f "$key_file" ]; then
+    key=$(openssl rand -hex 32)
+    echo "$key" >"$key_file" || {
+      echo -e "${RED}Error creando el archivo de clave${NC}"
+      exit 1
+    }
+  fi
 }
 
 generate_random_password() {
@@ -64,40 +77,47 @@ generate_random_password() {
 }
 
 create_db_creds_file() {
-  root_password=$(generate_random_password)
-  user_password=$(generate_random_password)
-  cat <<EOF >"./data/secrets/dbcreds.env"
+  creds_file="./data/secrets/dbcreds.env"
+  if [ ! -f "$creds_file" ]; then
+    root_password=$(generate_random_password)
+    user_password=$(generate_random_password)
+    cat <<EOF >"$creds_file"
 MYSQL_ROOT_PASSWORD=$root_password
 MYSQL_DATABASE=liberteis-db
 MYSQL_USER=liberteis-app
 MYSQL_PASSWORD=$user_password
 EOF
+  fi
 }
 
 create_init_indicator() {
-  touch "./data/init/initialized.txt" || {
-    echo "Error creando el archivo de indicador de inicialización"
-    exit 1
-  }
+  indicator_file="./data/init/initialized.txt"
+  if [ ! -f "$indicator_file" ]; then
+    touch "$indicator_file" || {
+      echo -e "${RED}Error creando el archivo de indicador de inicialización${NC}"
+      exit 1
+    }
+  fi
 }
 
 initialize() {
   if [ ! -f "./data/init/initialized.txt" ]; then
-    echo "Ejecutando inicialización..."
+    echo -e "${BLUE}Ejecutando inicialización...${NC}"
     create_directories
     set_key
     create_db_creds_file
     create_init_indicator
-    echo "Inicialización completa."
+    echo -e "${GREEN}Inicialización completa.${NC}"
   else
-    echo "Inicialización ya completada. Omitiendo."
+    echo -e "${YELLOW}Inicialización ya completada. Omitiendo.${NC}"
   fi
 }
 
 initialize
 
 if [ "$ENVIRONMENT" == "dev" ]; then
-  echo "Configurando entorno de desarrollo..."
+  clear
+  echo -e "${BLUE}Configurando entorno de desarrollo...${NC}"
   echo "Seleccione la opción de desarrollo:"
   echo "1) Fuera de container"
   echo "2) Dentro de container"
@@ -105,33 +125,36 @@ if [ "$ENVIRONMENT" == "dev" ]; then
 
   case $dev_choice in
   1)
-    echo "Ejecutando fuera de container..."
-    docker-compose -f docker-compose.dev.yml up -d mysql || {
-      echo "Error al iniciar MySQL con Docker Compose"
+    clear
+    echo -e "${BLUE}Ejecutando fuera de container...${NC}"
+    docker compose -f compose-db.yml up -d mysql || {
+      echo -e "${RED}Error al iniciar MySQL con Docker Compose${NC}"
       exit 1
     }
-    echo "Esperando a que MySQL esté saludable..."
+    echo -e "${BLUE}Esperando a que MySQL esté saludable...${NC}"
     until [ "$(docker inspect --format='{{.State.Health.Status}}' liberteis-app)" == "healthy" ]; do
       sleep 5
     done
-    echo "MySQL está saludable."
+    echo -e "${GREEN}MySQL está saludable.${NC}"
     ;;
   2)
-    echo "Ejecutando dentro de container..."
-    docker-compose -f docker-compose.dev.yml up --build || {
-      echo "Error al iniciar Docker Compose"
+    clear
+    echo -e "${BLUE}Ejecutando dentro de container...${NC}"
+    docker compose -f docker-compose.dev.yml up --build || {
+      echo -e "${RED}Error al iniciar Docker Compose${NC}"
       exit 1
     }
     ;;
   *)
-    echo "Opción no válida."
+    echo -e "${RED}Opción no válida.${NC}"
     exit 1
     ;;
   esac
 elif [ "$ENVIRONMENT" == "prod" ]; then
-  echo "Configurando entorno de producción..."
-  docker-compose -f docker-compose.prod.yml up || {
-    echo "Error al iniciar Docker Compose"
+  clear
+  echo -e "${BLUE}Configurando entorno de producción...${NC}"
+  docker compose -f docker-compose.prod.yml up || {
+    echo -e "${RED}Error al iniciar Docker Compose${NC}"
     exit 1
   }
 fi
