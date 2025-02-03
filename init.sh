950e938fa70ae5941c5fad23d1d0323f48aeff60
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-02-03 10:32:25 +0100

    fix(base): :bug: Now it rewrites the host env var instead of adding a new line

--- init.sh
+++ init.sh
@@ -94,10 +94,10 @@ EOF
 }
 
 # Add MySQL host to credentials
-add_mysql_host_to_creds() {
+update_mysql_host_in_creds() {
   creds_file="./data/secrets/dbcreds.env"
-  echo "MYSQL_HOST=$MYSQL_HOST" >>"$creds_file" || {
-    echo -e "${RED}Error adding MYSQL_HOST to credentials file${NC}"
+  sed -i "s/^MYSQL_HOST=.*/MYSQL_HOST=$MYSQL_HOST/" "$creds_file" || {
+    echo -e "${RED}Error updating MYSQL_HOST in credentials file${NC}"
     exit 1
   }
 }
@@ -122,6 +122,16 @@ set_mysql_host() {
   fi
 }
 
+# Stop and remove all containers
+stop_and_remove_containers() {
+  echo -e "${BLUE}Stopping and removing all containers...${NC}"
+  docker compose -f docker-compose.dev.yml down || {
+    echo -e "${RED}Error stopping and removing containers${NC}"
+    exit 1
+  }
+  echo -e "${GREEN}All containers stopped and removed.${NC}"
+}
+
 # Main initialization logic
 initialize() {
   if [ ! -f "./data/init/initialized.txt" ]; then
@@ -129,7 +139,6 @@ initialize() {
     create_directories
     set_key
     create_db_creds_file
-    create_init_indicator
     echo -e "${GREEN}Initialization complete.${NC}"
   else
     echo -e "${YELLOW}Initialization already completed. Skipping.${NC}"
@@ -142,41 +151,49 @@ initialize
 if [ "$ENVIRONMENT" == "dev" ]; then
   clear
   echo -e "${BLUE}Setting up development environment...${NC}"
-  echo "1) Outside container"
-  echo "2) Inside container"
-  read -p "Enter the corresponding number (1 or 2): " dev_choice
+  while true; do
+    echo "1) Outside container"
+    echo "2) Inside container"
+    echo "3) Stop and remove all containers"
+    read -p "Enter the corresponding number (1, 2, or 3): " dev_choice
 
-  case $dev_choice in
-  1)
-    clear
-    echo -e "${BLUE}Running outside container...${NC}"
-    set_mysql_host "local"
-    add_mysql_host_to_creds
-    docker compose -f compose-db.yml up -d mysql || {
-      echo -e "${RED}Error starting MySQL with Docker Compose${NC}"
-      exit 1
-    }
-    echo -e "${BLUE}Waiting for MySQL to become healthy...${NC}"
-    until [ "$(docker inspect --format='{{.State.Health.Status}}' liberteis-db)" == "healthy" ]; do
-      sleep 5
-    done
-    echo -e "${GREEN}MySQL is healthy.${NC}"
-    ;;
-  2)
-    clear
-    echo -e "${BLUE}Running inside container...${NC}"
-    set_mysql_host "container"
-    add_mysql_host_to_creds
-    docker compose -f docker-compose.dev.yml up --build || {
-      echo -e "${RED}Error starting Docker Compose${NC}"
-      exit 1
-    }
-    ;;
-  *)
-    echo -e "${RED}Invalid option.${NC}"
-    exit 1
-    ;;
-  esac
+    case $dev_choice in
+    1)
+      clear
+      echo -e "${BLUE}Running outside container...${NC}"
+      set_mysql_host "local"
+      update_mysql_host_in_creds
+      docker compose -f compose-db.yml up -d mysql || {
+        echo -e "${RED}Error starting MySQL with Docker Compose${NC}"
+        exit 1
+      }
+      echo -e "${BLUE}Waiting for MySQL to become healthy...${NC}"
+      until [ "$(docker inspect --format='{{.State.Health.Status}}' liberteis-db)" == "healthy" ]; do
+        sleep 5
+      done
+      echo -e "${GREEN}MySQL is healthy.${NC}"
+      break
+      ;;
+    2)
+      clear
+      echo -e "${BLUE}Running inside container...${NC}"
+      set_mysql_host "container"
+      update_mysql_host_in_creds
+      docker compose -f docker-compose.dev.yml up --build || {
+        echo -e "${RED}Error starting Docker Compose${NC}"
+        exit 1
+      }
+      break
+      ;;
+    3)
+      stop_and_remove_containers
+      break
+      ;;
+    *)
+      echo -e "${RED}Invalid option. Please try again.${NC}"
+      ;;
+    esac
+  done
 elif [ "$ENVIRONMENT" == "prod" ]; then
   clear
   echo -e "${BLUE}Setting up production environment...${NC}"
@@ -185,3 +202,6 @@ elif [ "$ENVIRONMENT" == "prod" ]; then
     exit 1
   }
 fi
+
+# Create initialization indicator at the end
+create_init_indicator
