f50cdac9354556a238332c4237a73765a1858649
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-04-07 22:03:42 +0200

    feat: :sparkles: Added loader overlay

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="flex h-screen">
     <SideBar />
-
     <div class="flex flex-col flex-1">
       <NavBar />
-
-      <main class="flex-1 overflow-auto">
-        <router-view />
-      </main>
+      <Suspense>
+        <template #default>
+          <main class="flex-1 overflow-auto">
+            <router-view />
+          </main>
+        </template>
+        <template #fallback>
+          <div class="overlay">
+            <span class="loader"></span>
+          </div>
+        </template>
+      </Suspense>
     </div>
   </div>
 </template>
@@ -17,4 +24,47 @@ import NavBar from './components/NavBar.vue';
 import SideBar from './components/SideBar.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #1e2939;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.loader {
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  display: block;
+  margin: 15px auto;
+  position: relative;
+  color: #FFF;
+  box-sizing: border-box;
+  animation: animloader 1s linear infinite alternate;
+}
+
+@keyframes animloader {
+  0% {
+    box-shadow: -38px -6px, -14px 6px, 14px -6px;
+  }
+
+  33% {
+    box-shadow: -38px 6px, -14px -6px, 14px 6px;
+  }
+
+  66% {
+    box-shadow: -38px -6px, -14px 6px, 14px -6px;
+  }
+
+  100% {
+    box-shadow: -38px 6px, -14px -6px, 14px 6px;
+  }
+}
+</style>
